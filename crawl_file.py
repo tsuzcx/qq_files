4d3a57f2943e6124db89891dd0402e45da384c5c
tsuzcx <98632993+tsuzcx@users.noreply.github.com>
2022-01-31 00:43:31 +0000

    add crawler

---
+++ crawl_file.py
@@ -0,0 +1,37 @@
+import os,re
+from urllib import parse
+
+def covert_path(path:str) -> str:
+    return re.sub(r'[:*?\'"<>|]','_',path)
+
+def crawl_one(url:str):
+    s = parse.urlsplit(url)
+    t = [s.netloc]
+    if s.path:
+        t.extend(s.path.split('/'))
+    if len(t) == 1:
+        t.append('index.html')
+    path = os.path.join(*t)
+    dn,fn = os.path.split(path)
+    os.makedirs(dn,exist_ok=True)
+    if s.query:
+        q = s.query
+        try:
+            t1,t2 = fn.rsplit('.')
+            fn = f'{t1}_{q}{t2}'
+        except ValueError:
+            fn = f'{fn}_{q}'
+        path = os.path.join(dn,fn)
+    path = covert_path(path)
+    cmd = f"axel -o '{path}' '{url}'"
+    print(cmd)
+    os.system(cmd)
+
+def main():
+    while 1:
+        url = input()
+        if not url:break
+        crawl_one(url)
+
+if __name__ == '__main__':
+    main()
